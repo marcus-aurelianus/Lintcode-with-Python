2cfefd96d4df9134d0a45b886663a1b16701e2bc
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2018-06-25 16:09:43 +0800

    Create alternate sol(fast).py

---
+++ 3. Digit Counts/alternate sol(fast).py
@@ -0,0 +1,29 @@
+class Solution:
+    """
+    @param: : An integer
+    @param: : An integer
+    @return: An integer denote the count of digit k in 1..n
+    """
+
+    def digitCounts(self, k, n):
+        def helper(k,n):
+            if k==0 and n==0:
+                return 1
+            res=0
+            while n!=0:
+                res+=(n%10==k)
+                n//=10
+            return res
+        d=n//10
+        u=n%10
+        if n<10:
+            return 1
+        if u==9:
+            temp=self.digitCounts(k,d)
+            return temp*10+d+1 if k!=0 else (temp-1)*10+d+1
+        return self.digitCounts(k,n-(u+1))+helper(k,d)*(u+1)+(u>k-1)
+
+#Test Cases:
+    
+sol=Solution()
+print(sol.digitCounts(0,123))
