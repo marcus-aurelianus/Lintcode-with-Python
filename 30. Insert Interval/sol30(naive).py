7914f3c862b26f6a246cb1a3a87f953782c0d8de
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2018-06-28 16:57:30 +0800

    Create sol30(naive).py

---
+++ 30. Insert Interval/sol30(naive).py
@@ -0,0 +1,41 @@
+class Interval:
+    def __init__(self,start,end):
+        self.start=start
+        self.end=end
+class Solution:
+    def insert(self,intervals,newInterval):
+        res=[]
+        pos=0
+        temp=[]
+        for interval in intervals:
+            if interval.end==newInterval.start:
+                temp.append(interval.start)
+                temp.append(newInterval.end)
+            elif interval.start==newInterval.end:
+                if temp:
+                    temp.pop()
+                    temp.append(interval.end)
+                else:
+                    temp.append(newInterval.start)
+                    temp.append(interval.end)
+            else:
+                if temp:
+                    res.append(Interval(temp[0],[1]))
+                    temp=[]
+                else:
+                    if interval.start>newInterval.end:
+                        res.append(newInterval)
+                    res.append(interval)
+        if temp:
+            res.append(Interval(temp[0],[1]))
+        return res
+#Test Case
+interval_a=Interval(1,2)
+interval_b=Interval(5,9)
+intervals=(interval_a,interval_b)
+sol=Solution()
+new=Interval(3,4)
+c=sol.insert(intervals,new)
+print(c)
+for k in c:
+    print(k.start,k.end)
