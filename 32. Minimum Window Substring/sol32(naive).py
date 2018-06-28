1e3acd237a5da04b0138b56aa0ef05170ca8583b
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2018-06-28 17:35:53 +0800

    Create sol32(naive).py

---
+++ 32. Minimum Window Substring/sol32(naive).py
@@ -0,0 +1,22 @@
+class Solution:
+    def minWindow(self,source,target):
+        start,end=0,len(source)
+        def helper(str1,str2):
+            for ele in str2:
+                if str2.count(ele)>str1.count(ele) or ele not in str1:
+                    return False
+            return True
+        while start<=end and helper(source[start:end],target):
+            start+=1
+        start-=1
+        while start<=end and helper(source[start:end],target):
+            end-=1
+        end+=1
+        return source[start:end]
+
+
+#Test Case
+source='AD0BEC0DEBANC'
+target="ABC"
+sol=Solution()
+print(sol.minWindow(source,target))
