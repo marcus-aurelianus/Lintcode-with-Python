07554f7cfea83ca86fcd6306266ee83ff6004f6a
Marcus.Aurelianus <marcus.lihao@gmail.com>
2018-07-01 15:44:50 +0800

    sol40

---
+++ 40. Implement Queue by Two Stacks/sol40.py
@@ -0,0 +1,36 @@
+class MyQueue:
+    
+    def __init__(self):
+        self.stk1=[]
+        self.stk2=[]
+    """
+    @param: element: An integer
+    @return: nothing
+    """
+    def push(self, element):
+        self.stk1.append(element)
+
+    """
+    @return: An integer
+    """
+    def pop(self):
+        if self.stk2==[]:
+            while self.stk1!=[]:
+                ele=self.stk1.pop()
+                self.stk2.append(ele)
+        return self.stk2.pop()
+    """
+    @return: An integer
+    """
+    def top(self):
+        while self.stk1!=[]:
+            ele=self.stk1.pop()
+            self.stk2.append(ele)
+        return self.stk2[-1]
+
+
+# Test case:
+sol=MyQueue()
+sol.push(1)
+print(sol.pop())
+
