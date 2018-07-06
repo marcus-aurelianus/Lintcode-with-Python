762b52b52153bde8709634d127b7885f6359156d
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2018-07-06 09:43:26 +0800

    Update sol51(abbr).py

--- 51. Previous Permutation/sol51(abbr).py
+++ 51. Previous Permutation/sol51(abbr).py
@@ -5,12 +5,10 @@ class Solution:
         # write your code here
         for i in range(len(num)-2, -1, -1):
             if num[i] > num[i+1]:
-                print(1)
                 break
-            else:
-                print(1)
-                num.reverse()
-                return num
+        else:
+            num.reverse()
+            return num
         for j in range(len(num)-1, i, -1):
             if num[j] < num[i]:
                 num[i], num[j] = num[j], num[i]
