576c0b5aa19d971edc4bb4e68fbc350dd8525a7c
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2018-07-16 16:23:21 +0800

    Create sol61.py

---
+++ 61. Search for a Range/sol61.py
@@ -0,0 +1,31 @@
+class Solution:
+    def searchRange(self, A, target):
+        if len(A) == 0:
+            return [-1, -1]
+        
+        start1, end1 = 0, len(A) - 1
+        start2, end2 = 0, len(A) - 1
+        while start1 < end1-1 or start2<end2-1:
+            mid1=(start1+end1)//2
+            mid2=(start2+end2)//2
+            if A[mid2]>target:
+                end2=mid2
+            else:
+                start2=mid2
+            if A[mid1]>=target:
+                end1=mid1
+            else:
+                start1=mid1
+        if [start1,end1]==[start2,end2]:
+            if A[end1]==target:
+                return [end1,end1]
+            else:
+                return [start1,start1]
+        elif A[end1]==target:   
+            return [end1,start2]
+        else:
+            return [-1,-1]
+
+#Test Case:
+sol=Solution()
+print(sol.searchRange([5,7,7,8,8,10],5))
