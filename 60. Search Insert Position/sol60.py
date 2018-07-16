746fd874aa900e5fb9026c15f717e990c0b19d3a
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2018-07-16 14:40:58 +0800

    Update solution (previous is matching value)...

--- 60. Search Insert Position/sol60.py
+++ 60. Search Insert Position/sol60.py
@@ -1,20 +1,18 @@
 class Solution:
     def SearchInsert(self,A,target):
         start,end=0,len(A)-1
-        while start<end:
+        while start<end-1:
             mid=(start+end)//2
-            if A[start]==target:
-                return start
-            elif A[end]==target:
-                return end
+            if A[mid]>target:
+                end=mid
             else:
-                if A[mid]==target:
-                    return mid
-                elif A[mid]>target:
-                    end=mid-1
-                else:
-                    start=mid+1
-        return 0
+                start=mid
+        if target>A[end]:
+            return end+1
+        elif target>A[start]:
+            return end
+        else:
+            return start
 #Test Case:
 sol=Solution()
-print(sol.SearchInsert([1,3,5,6],1))
+print(sol.SearchInsert([1,3,5,6],3))
